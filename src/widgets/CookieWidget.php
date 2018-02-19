f9a7a59602a6110dbfc88dbb210f377efa4b4573
Albert Borsos <albertborsos@me.com>
2018-02-19 16:11:53 +0100

    set more options in preparePluginOptions method

--- src/widgets/CookieWidget.php
+++ src/widgets/CookieWidget.php
@@ -5,6 +5,7 @@ namespace albertborsos\cookieconsent\widgets;
 use albertborsos\cookieconsent\assets\CookieConsentAsset;
 use Yii;
 use yii\base\Widget;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use yii\helpers\Url;
 use yii\web\View;
@@ -26,10 +27,24 @@ class CookieWidget extends Widget
     const COMPLIANCE_TYPE_OPT_IN = 'opt-in';
     const COMPLIANCE_TYPE_OPT_OUT = 'opt-out';
 
+    const DEFAULT_PALETTE = [
+        'popup' => [
+            'background' => '#000',
+        ],
+        'button' => [
+            'background' => '#f1d600',
+        ],
+    ];
+
     /**
      * @var string
      */
     public $position = self::POSITION_BOTTOM;
+
+    /**
+     * @var array
+     */
+    public $paletteConfig = [];
 
     /**
      * @var string
@@ -87,6 +102,31 @@ class CookieWidget extends Widget
 
     private function preparePluginOptions()
     {
+        if (!empty($this->position)) {
+            $this->pluginOptions['position'] = $this->position;
+
+            if ($this->position === self::POSITION_TOP_PUSHDOWN) {
+                $this->pluginOptions['position'] = self::POSITION_TOP;
+                $this->pluginOptions['static'] = true;
+            }
+        }
+
+        if (empty(ArrayHelper::getValue($this->pluginOptions, 'palette'))) {
+            $this->pluginOptions['palette'] = self::DEFAULT_PALETTE;
+        }
+
+        if (!empty($this->paletteConfig)) {
+            $this->pluginOptions['palette'] = $this->paletteConfig;
+        }
+
+        if (!empty($this->layout)) {
+            $this->pluginOptions['layout'] = $this->layout;
+        }
+
+        if (!empty($this->complianceType)) {
+            $this->pluginOptions['type'] = $this->complianceType;
+        }
+
         if (!empty($this->message)) {
             $this->pluginOptions['content']['message'] = $this->message;
         }
