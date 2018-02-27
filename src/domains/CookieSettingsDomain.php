b0030a62f33492395d19e83474f5835426030c90
Albert Borsos <albertborsos@me.com>
2018-02-27 11:46:39 +0100

    add initial settings form and domain

---
+++ src/domains/CookieSettingsDomain.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace albertborsos\cookieconsent\domains;
+
+use albertborsos\ddd\models\AbstractDomain;
+
+class CookieSettingsDomain extends AbstractDomain
+{
+
+    /**
+     * Business logic to store data for multiple resources.
+     *
+     * @return mixed
+     */
+    public function process()
+    {
+        // TODO: Implement process() method.
+    }
+}
---
+++ src/domains/forms/CookieSettingsForm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace albertborsos\cookieconsent\domains\forms;
+
+use albertborsos\ddd\interfaces\FormObject;
+use yii\base\Model;
+
+class CookieSettingsForm extends Model implements FormObject
+{
+    public function rules()
+    {
+        return [];
+    }
+}
