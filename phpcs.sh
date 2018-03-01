6a48c6062ba15876ac5154b9fa932a45af811aba
Albert Borsos <albertborsos@me.com>
2018-03-01 12:09:13 +0100

    update phpcs Views ruleset to ViewsMixed

--- phpcs.sh
+++ phpcs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Application src --ignore=*/views/* --extensions=php
+php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Application --ignore=*/views/* --extensions=php /src
 SRC=$?
-php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Views src/actions/views --extensions=php
+php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/ViewsMixed --extensions=php /src/actions/views
 VIEWS=$?
-php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Application -s --exclude=PSR1.Files.SideEffects,PSR1.Classes.ClassDeclaration --extensions=php  tests
+php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Application -s --exclude=PSR1.Files.SideEffects,PSR1.Classes.ClassDeclaration --extensions=php  /tests
 TESTS=$?
 
 if [ $SRC -ne 0 ] || [ $VIEWS -ne 0 ] || [ $TESTS -ne 0 ]; then
