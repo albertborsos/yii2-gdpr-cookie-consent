88d7bdc858e9db42d477e352fc1d889a9af2f51a
Albert Borsos <albertborsos@me.com>
2018-03-01 12:02:50 +0100

    add view files path to phpcs

--- phpcs.sh
+++ phpcs.sh
@@ -2,9 +2,11 @@
 
 php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Application src --ignore=*/views/* --extensions=php
 SRC=$?
+php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Views src/actions/views --extensions=php
+VIEWS=$?
 php ./vendor/bin/phpcs --standard=vendor/mito/yii2-coding-standards/Application -s --exclude=PSR1.Files.SideEffects,PSR1.Classes.ClassDeclaration --extensions=php  tests
 TESTS=$?
 
-if [ $SRC -ne 0 ] || [ $TESTS -ne 0 ]; then
+if [ $SRC -ne 0 ] || [ $VIEWS -ne 0 ] || [ $TESTS -ne 0 ]; then
     exit 1
 fi
